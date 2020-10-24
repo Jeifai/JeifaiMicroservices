9161174ee740d6f50b639a53d9d14da1312a206a
Roberto Malcotti <roberto.malcotti@datangels.com>
2020-10-25 01:09:03 +0200

    working function

--- extract_country_and_city/main.py
+++ extract_country_and_city/main.py
@@ -1,4 +1,29 @@
+import sys
 import json
+import nltk
+import geograpy
+from geograpy.extraction import Extractor
+
+sys.path.append('lib/')
+nltk.data.path.append('lib/nltk_data/')
+nltk.download('punkt')
+nltk.download('words')
+nltk.download('treebank')
+nltk.download('maxent_ne_chunker')
+nltk.download('averaged_perceptron_tagger')
+nltk.download('maxent_treebank_pos_tagger')
 
 def extract_country_and_city(request):
-	return json.dumps({'status': 'success'})
+    input = request.args.get('input')
+    countries = []
+    e=Extractor(text=input)
+    places=e.find_entities()
+    for place in places:
+        city=geograpy.locateCity(place)
+        country=city.country.iso
+        countries.append(country)
+    s_countries = ','.join(list(set(countries)))
+    return json.dumps({"status": "success","countries": s_countries})
+
+if __name__ == "__main__":
+	print(extract_country_and_city("Welcome in Milan"))
