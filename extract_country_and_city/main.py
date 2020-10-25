dbbc0f30cd209e1be66a29d6648260a3e0c6094b
robimalco <robimalco@gmail.com>
2020-10-25 16:23:39 +0000

    add db

--- extract_country_and_city/main.py
+++ extract_country_and_city/main.py
@@ -7,13 +7,10 @@ from geograpy.extraction import Extractor
 sys.path.append('lib/')
 nltk.data.path.append('lib/nltk_data/')
 
-
 def extract_country_and_city(request):
     input = str(request.args.get('input'))
     e=Extractor(text=input)
     places = e.find_entities()
-    return json.dumps({"status": "success","countries": places})
-    '''
     countries = []
     for place in places:
         city=geograpy.locateCity(place)
@@ -21,7 +18,6 @@ def extract_country_and_city(request):
         countries.append(country)
     s_countries = ','.join(list(set(countries)))
     return json.dumps({"status": "success","countries": s_countries})
-    '''
 
 if __name__ == "__main__":
 	print(extract_country_and_city("Welcome in Milan"))
